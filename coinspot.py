39d2f6fb408c059d184fd640e9fcc621730a36c2
rohan-bhatia <38775995+rohan-bhatia@users.noreply.github.com>
2018-04-27 19:57:56 +1000

    Update coinspot.py
    - Added sell price for top 20 coins on Coinspot and a few others.
    - Added a input at the end to avoid console closing automatically

--- coinspot.py
+++ coinspot.py
@@ -8,24 +8,35 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
-
+#List of URLs to scrap
 def getCoinSpotWebsiteURLS():
     websiteURLList = []
-    websiteURLList.append('https://www.coinspot.com.au/sell/ADA')
-    websiteURLList.append('https://www.coinspot.com.au/sell/BTC')
-    websiteURLList.append('https://www.coinspot.com.au/sell/CAN')
-    websiteURLList.append('https://www.coinspot.com.au/sell/DENT')
-    websiteURLList.append('https://www.coinspot.com.au/sell/DOGE')
-    websiteURLList.append('https://www.coinspot.com.au/sell/ETH')
-    websiteURLList.append('https://www.coinspot.com.au/sell/FUN')
-    websiteURLList.append('https://www.coinspot.com.au/sell/RDD')
-    websiteURLList.append('https://www.coinspot.com.au/sell/STR')
-    websiteURLList.append('https://www.coinspot.com.au/sell/TRX')
-    websiteURLList.append('https://www.coinspot.com.au/sell/XRP')
-    websiteURLList.append('https://www.coinspot.com.au/sell/XVG')
+	websiteURLList.append('https://www.coinspot.com.au/sell/BTC')
+	websiteURLList.append('https://www.coinspot.com.au/sell/ETH')
+	websiteURLList.append('https://www.coinspot.com.au/sell/XRP')
+	websiteURLList.append('https://www.coinspot.com.au/sell/BCC')
+	websiteURLList.append('https://www.coinspot.com.au/sell/EOS')
+	websiteURLList.append('https://www.coinspot.com.au/sell/LTC')
+	websiteURLList.append('https://www.coinspot.com.au/sell/STR')
+	websiteURLList.append('https://www.coinspot.com.au/sell/ADA')
+	websiteURLList.append('https://www.coinspot.com.au/sell/MIOTA')
+	websiteURLList.append('https://www.coinspot.com.au/sell/NEO')
+	websiteURLList.append('https://www.coinspot.com.au/sell/TRX')
+	websiteURLList.append('https://www.coinspot.com.au/sell/XMR')
+	websiteURLList.append('https://www.coinspot.com.au/sell/DASH')
+	websiteURLList.append('https://www.coinspot.com.au/sell/XEM')
+	websiteURLList.append('https://www.coinspot.com.au/sell/USDT')
+	websiteURLList.append('https://www.coinspot.com.au/sell/ETC')
+	websiteURLList.append('https://www.coinspot.com.au/sell/VEN')
+	websiteURLList.append('https://www.coinspot.com.au/sell/OMG')
+	websiteURLList.append('https://www.coinspot.com.au/sell/QTUM')
+	websiteURLList.append('https://www.coinspot.com.au/sell/ICX')
+	websiteURLList.append('https://www.coinspot.com.au/sell/XVG')
+	websiteURLList.append('https://www.coinspot.com.au/sell/XRB')
+	websiteURLList.append('https://www.coinspot.com.au/sell/DOGE')
+	websiteURLList.append('https://www.coinspot.com.au/sell/PIVX')
+	websiteURLList.append('https://www.coinspot.com.au/sell/CAN')
     return websiteURLList
-
-priceListForFinalPrint = []
 
 print("---------------------")
 print("Format: Name = Price")
@@ -37,5 +48,7 @@ for websiteURL in getCoinSpotWebsiteURLS():
     priceStringFull = soup.select('h1.price-title')[0].text.strip()
     priceString = priceStringFull.split("$")
     print(priceString[0] + priceString[1])
-    
-    priceListForFinalPrint.append(priceString[1])
+
+print("")
+print("")	
+input("Press enter to exit...")
